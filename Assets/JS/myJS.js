341747a233837798b34ea7e7cf3f73f849752c25
DuNguyen <dunguyen@bluecoral.com.vn>
2023-03-18 18:55:45 +0700

    slider fixed

--- Assets/JS/myJS.js
+++ Assets/JS/myJS.js
@@ -1,43 +1,78 @@
-var owl = $(".owl-carousel");
+// var owl = $(".owl-carousel");
 
-owl.owlCarousel(
-  $(document).ready(function(){
-    $('.loop').owlCarousel({
-        center: false,
-        items:3,
-        loop:true,
-        margin: 50,
-        // navContainer: ".arrows",
-        dotsContainer: ".dots",
-        dotsEach: true,
-        responsive:{
-            0: {
-              items: 1,
-            },
-            700: {             
-              items: 2,
-            },
-            1000: {
-              margin: 300,
-              items: 3,
-            },
-          },
-    });
-  })
-);
+// owl.owlCarousel(
+//   $(document).ready(function(){
+//     $('.loop').owlCarousel({
+//         center: false,
+//         items:3,
+//         loop:true,
+//         margin: 50,
+//         // navContainer: ".arrows",
+//         dotsContainer: ".dots",
+//         dotsEach: true,
+//         responsive:{
+//             0: {
+//               items: 1,
+//             },
+//             700: {             
+//               items: 2,
+//             },
+//             1000: {
+//               margin: 300,
+//               items: 3,
+//             },
+//           },
+//     });
+//   })
+// );
 
-// Go to the next item
-$(".container10_controllers-right-arrow").click(function() {
-    owl.trigger('next.owl.carousel');
-})
+// // Go to the next item
+// $('.container10_controllers-right-arrow').click(function() {
+//   owl.trigger('next.owl.carousel');
+// })
+
+// // Go to the previous item
+// $(".container10_controller-left-arrow").click(function() {
+//     owl.trigger('prev.owl.carousel', [300]);
+// })
 
 
-// Go to the previous item
-$(".container10_controller-left-arrow").click(function() {
-    owl.trigger('prev.owl.carousel', [300]);
-})
+// $(".dot").click(function() {
+//   owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+// })
 
 
-$(".dot").click(function() {
-  owl.trigger('to.owl.carousel', [$(this).index(), 300]);
-})
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  loop: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {    
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  },
+  navigation: {
+    nextEl: ".container10_controllers-right-arrow",
+    prevEl: ".container10_controllers-left-arrow",
+  },
+
+});
+
